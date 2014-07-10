8128dd25fb8c2493f8b41d07f98a0556135edba3
hkellaway <hkellaway.tech@gmail.com>
2014-07-10 10:52:18 -0400

    Starts fleshing out MainScene.m - didLoadFromCCB, update, touchBegan

--- harlankellaway.spritebuilder/Source/MainScene.m
+++ harlankellaway.spritebuilder/Source/MainScene.m
@@ -9,5 +9,35 @@
 #import "MainScene.h"
 
 @implementation MainScene
+{
+    CCSprite *_socialMediaStream;
+}
+
+- (void)didLoadFromCCB
+{
+    
+    // tell this scene to accept touches
+    self.userInteractionEnabled = YES;
+}
+
+- (void)update:(CCTime)delta
+{
+    // TODO: scrolling functionality to be implemented here
+}
+
+// called on every touch in this scene
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    // touch location inside Social Media Stream
+    CGPoint touchLocation = [touch locationInNode:_socialMediaStream];
+    
+    CCLOG(@"Bounding box of SocialMediaStream: (%f, %f)", [_socialMediaStream boundingBox].origin.x, [_socialMediaStream boundingBox].origin.y);
+    
+    // start catapult dragging when a touch inside of the catapult arm occurs
+    if (CGRectContainsPoint([_socialMediaStream boundingBox], touchLocation))
+    {
+        CCLOG(@"Touch made in Social Media Stream: (%f, %f)", touchLocation.x, touchLocation.y);
+    }
+}
 
 @end
