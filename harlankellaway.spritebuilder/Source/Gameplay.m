93b827344e6e9661aee00c3df776da7b0e06c6ad
hkellaway <hkellaway.tech@gmail.com>
2014-07-10 13:05:08 -0400

    Scales SocialMediaStream area to be acted on when touch occurs

--- harlankellaway.spritebuilder/Source/Gameplay.m
+++ harlankellaway.spritebuilder/Source/Gameplay.m
@@ -13,30 +13,45 @@
     CCSprite *_socialMediaStream;
 }
 
-//- (void)didLoadFromCCB
-//{
-//    
-//    // tell this scene to accept touches
-//    self.userInteractionEnabled = YES;
-//}
-//
-//- (void)update:(CCTime)delta
-//{
-//    // TODO: scrolling functionality to be implemented here
-//}
-//
-//// called on every touch in this scene
-//- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
-//{
-//    // touch location inside Social Media Stream
-//    CGPoint touchLocation = [touch locationInNode:_socialMediaStream];
-//    
-//    CCLOG(@"Bounding box of SocialMediaStream: (%f, %f)", [_socialMediaStream boundingBox].origin.x, [_socialMediaStream boundingBox].origin.y);
-//    
-//    // if touch is inside the SocialMedia Stream...
-//    if (CGRectContainsPoint([_socialMediaStream boundingBox], touchLocation))
-//    {
-//        CCLOG(@"Touch made in Social Media Stream: (%f, %f)", touchLocation.x, touchLocation.y);
-//    }
-//}
+- (void)didLoadFromCCB
+{
+    
+    // tell this scene to accept touches
+    self.userInteractionEnabled = YES;
+}
+
+- (void)update:(CCTime)delta
+{
+    // TODO: scrolling functionality to be implemented here
+}
+
+// called on every touch in this scene
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    // touch location inside Social Media Stream
+    CGPoint touchLocation = [touch locationInNode:self];
+    
+    // bounding box and size need to be scaled
+    CGRect originalBoundingBox = [_socialMediaStream boundingBox];
+    CGSize originalSize = _socialMediaStream.contentSize;
+    
+    // scaling amounts
+    CGFloat scaleX = _socialMediaStream.scaleX;
+    CGFloat scaleY = _socialMediaStream.scaleY;
+    
+    CGFloat x = (originalBoundingBox.origin.x) * scaleX;
+    CGFloat y = (originalBoundingBox.origin.y) * scaleY;
+    CGFloat width = (originalSize.width) * scaleX;
+    CGFloat height = (originalSize.height) * scaleY;
+    
+    CGRect scaledBoundingBox = CGRectMake(x, y, width, height);
+    
+    // if touch is inside the SocialMedia Stream...
+    if (CGRectContainsPoint(scaledBoundingBox, touchLocation))
+    {
+        CCLOG(@"Touch made in Social Media Stream: (%f, %f)", touchLocation.x, touchLocation.y);
+        
+        // TODO: determine which SocialMediaStatus was touched
+    }
+}
 @end
