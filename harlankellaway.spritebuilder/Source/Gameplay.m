43123e31469a602e40832c83a04dd9d4a437dc81
Harlan Kellaway <hello@harlankellaway.com>
2014-07-14 09:46:52 -0400

    Changes background image from jpeg to png

--- harlankellaway.spritebuilder/Source/Gameplay.m
+++ harlankellaway.spritebuilder/Source/Gameplay.m
@@ -12,15 +12,11 @@
 static const int NUM_STATUSES = 12;
 static const int NUM_ACTION_STATES = 3;
 
-static const int NUM_STATUS_OPTIONS = 5;
-
 @implementation Gameplay
 {
     CCLabelTTF *_messageNumber;
     
     SocialMediaStatus *_statuses[NUM_STATUSES];
-    
-    NSString *_statusTextOptions[NUM_STATUS_OPTIONS];
 }
 
 - (void)didLoadFromCCB
@@ -32,21 +28,13 @@ static const int NUM_STATUS_OPTIONS = 5;
     CGFloat height = 44;
     CGFloat spacing = 12;
     
-    
-    // TODO: don't hardcode status options
-    _statusTextOptions[0] = @"Buckets are so hilarious!";
-    _statusTextOptions[1] = @"Most trees are blue..";
-    _statusTextOptions[2] = @"Are snakes real?";
-    _statusTextOptions[3] = @"Daft punk is dope.";
-    _statusTextOptions[4] = @"I love straws";
-    
     for(int i = 0; i < NUM_STATUSES; i++)
     {
         SocialMediaStatus *status = (SocialMediaStatus*)[CCBReader load:@"SocialMediaStatus"];
         
         status.position = ccp(xPos, (yStart - (i * height)) + spacing);
         
-        status.statusText.string = _statusTextOptions[0 + arc4random() % (NUM_STATUS_OPTIONS)];
+        status.statusText.string = [self getRandomStatus];
         
 //        status.actionType = 0 + arc4random() % (NUM_ACTION_STATES);
         
@@ -95,6 +83,22 @@ static const int NUM_STATUS_OPTIONS = 5;
 - (void)checkMessages
 {
     CCLOG(@"Message button pressed");
+}
+
+- (NSString*)getRandomStatus
+{
+    int numStatuses = 5;
+    
+    NSString *_statusTextOptions[numStatuses];
+    
+    // TODO: don't hardcode status options
+    _statusTextOptions[0] = @"Buckets are so hilarious!";
+    _statusTextOptions[1] = @"Most trees are blue..";
+    _statusTextOptions[2] = @"Are snakes real?";
+    _statusTextOptions[3] = @"Daft punk is dope.";
+    _statusTextOptions[4] = @"I love straws";
+    
+    return _statusTextOptions[0 + arc4random() % (numStatuses)];
 }
 
 @end
