4976bce13e298a334e39b9a5af22e129296d4306
Harlan Kellaway <hello@harlankellaway.com>
2014-07-17 13:29:20 -0400

    Adds Trend class to SB and Xcode

---
+++ harlankellaway.spritebuilder/Source/Trend.h
@@ -0,0 +1,13 @@
+//
+//  Trend.h
+//  harlankellaway
+//
+//  Created by Harlan Kellaway on 7/17/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Trend : CCSprite
+
+@end
---
+++ harlankellaway.spritebuilder/Source/Trend.m
@@ -0,0 +1,13 @@
+//
+//  Trend.m
+//  harlankellaway
+//
+//  Created by Harlan Kellaway on 7/17/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Trend.h"
+
+@implementation Trend
+
+@end
