453baf3b78d64735713193f824d9b1ff4c5d1551
Harlan Kellaway <hello@harlankellaway.com>
2014-07-23 23:36:41 -0400

    Adds plain Table View Controller and canned Tweet text

---
+++ IDOL OnDemand Tutorial/IDOL OnDemand Tutorial/IDOL-TUTTweetsViewController.m
@@ -0,0 +1,127 @@
+//
+//  IDOL-TUTTweetsViewController.m
+//  IDOL OnDemand Tutorial
+//
+//  Created by Harlan Kellaway on 7/23/14.
+//  Copyright (c) 2014 ___HARLANKELLAWAY___. All rights reserved.
+//
+
+#import "IDOL-TUTTweetsViewController.h"
+
+@interface IDOL_TUTTweetsViewController ()
+
+@end
+
+@implementation IDOL_TUTTweetsViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.tweets = [self getTweetsWithHastag:@"F1"];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [self.tweets count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // set up cell
+    static NSString *CellIdentifier = @"Cell Identifier";
+    [tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:CellIdentifier];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // set cell text to tweet
+    NSString *tweet = [self.tweets objectAtIndex:[indexPath row]];
+    [cell.textLabel setText:tweet];
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark - Helper methods
+
+- (NSMutableArray *)getTweetsWithHastag:(NSString *)hastag
+{
+    NSMutableArray *tweets = [NSMutableArray array];
+    [tweets addObject:@"I love cats"];
+    [tweets addObject:@"I like dogs"];
+    [tweets addObject:@"I like dogs and I hate cats."];
+    
+    return tweets;
+}
+
+@end
