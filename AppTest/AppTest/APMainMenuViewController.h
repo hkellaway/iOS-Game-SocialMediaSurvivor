75094044b8ea5e8615d4dc3668cea6010ec6f7cc
Harlan Kellaway <hello@harlankellaway.com>
2014-07-26 14:00:13 -0400

    Creates classes for first two screens

---
+++ AppTest/AppTest/APMainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  APMainMenuViewController.h
+//  AppTest
+//
+//  Created by Harlan Kellaway on 7/26/14.
+//  Copyright (c) 2014 ___HARLANKELLAWAY___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APMainMenuViewController : UIViewController
+
+@end
---
+++ AppTest/AppTest/APMainMenuViewController.m
@@ -0,0 +1,49 @@
+//
+//  APMainMenuViewController.m
+//  AppTest
+//
+//  Created by Harlan Kellaway on 7/26/14.
+//  Copyright (c) 2014 ___HARLANKELLAWAY___. All rights reserved.
+//
+
+#import "APMainMenuViewController.h"
+
+@interface APMainMenuViewController ()
+
+@end
+
+@implementation APMainMenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
