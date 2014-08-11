cc6d2c2b4126a1196caf3a0071fc9e67e7bfe754
Harlan Kellaway <hello@harlankellaway.com>
2014-08-11 15:50:41 -0400

    Implements new LevelIntro screen design

--- harlankellaway.spritebuilder/Source/LevelIntroScene.m
+++ harlankellaway.spritebuilder/Source/LevelIntroScene.m
@@ -8,13 +8,19 @@
 
 #import "LevelIntroScene.h"
 #import "Trend.h"
+#import "CCTextureCache.h"
 #import "Utilities.h"
 #import "GameState.h"
 
 @implementation LevelIntroScene
 {
+    // Declared in SpriteBuilder
     CCLabelTTF *_levelLabel;
-    CCNode *_levelIntroTrendsBox;
+    CCNode *_levelIntroRecirculateBox;
+    CCNode *_levelIntroFavoriteBox;
+    CCNode *_imageRecirculate;
+    CCNode *_imageFavorite;
+    //////////////////////////////
     
     NSMutableArray *_allTopics;
     NSMutableSet *_usedTopics;
@@ -25,7 +31,10 @@
 - (void)didLoadFromCCB
 {
     int levelNum = [GameState sharedInstance].levelNum;
-
+    
+    _imageRecirculate.visible = FALSE;
+    _imageFavorite.visible = FALSE;
+    
     _allTopics = [NSMutableArray array];
     _usedTopics = [NSMutableSet set];
     
@@ -34,7 +43,7 @@
     _numToFavorite = [[levelsArray[levelNum - 1] objectForKey:@"NumTopicsToFavorite"] intValue];
     
     // read in current Level and set Scene title
-    _levelLabel.string = [NSString stringWithFormat:@"Day %d", [GameState sharedInstance].levelNum];
+    _levelLabel.string = [NSString stringWithFormat:@"Day %ld", (long)[GameState sharedInstance].levelNum];
     
     NSMutableArray *tempTopics = [[NSMutableArray alloc] init];
     
@@ -84,7 +93,7 @@
         Trend *trend = (Trend *)[CCBReader load:@"Trend"];
         [trend setTrendText:[NSString stringWithFormat:@"%@", ((NSString *)trendsToFavorite[j]).capitalizedString]];
         [trend setTrendAction:[GameState sharedInstance].imageNameFavorite];
-        [_levelIntroTrendsBox addChild:trend];
+        [_levelIntroFavoriteBox addChild:trend];
     }
     
     for(int i = 0; i < [trendsToRecirculate count]; i++)
@@ -92,7 +101,22 @@
         Trend *trend = (Trend *)[CCBReader load:@"Trend"];
         [trend setTrendText:[NSString stringWithFormat:@"%@", ((NSString *)trendsToRecirculate[i]).capitalizedString]];
         [trend setTrendAction:[GameState sharedInstance].imageNameRecirculate];
-        [_levelIntroTrendsBox addChild:trend];
+        [_levelIntroRecirculateBox addChild:trend];
+    }
+    
+    // add images
+    if(_numToRecirculate > 0)
+    {
+        [_levelIntroRecirculateBox removeChild:_imageRecirculate];
+        [_levelIntroRecirculateBox addChild:_imageRecirculate];
+        _imageRecirculate.visible = TRUE;
+    }
+    
+    if(_numToFavorite > 0)
+    {
+        [_levelIntroFavoriteBox removeChild:_imageFavorite];
+        [_levelIntroFavoriteBox addChild:_imageFavorite];
+        _imageFavorite.visible = TRUE;
     }
 }
 
