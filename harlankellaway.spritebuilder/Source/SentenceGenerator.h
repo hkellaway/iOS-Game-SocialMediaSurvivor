ca505e99dba3f2d84b9c528fa181554e0bc0d977
Harlan Kellaway <hello@harlankellaway.com>
2014-08-12 10:46:01 -0400

    Starts work on Sentence Generator

---
+++ harlankellaway.spritebuilder/Source/SentenceGenerator.h
@@ -0,0 +1,13 @@
+//
+//  SentenceGenerator.h
+//  harlankellaway
+//
+//  Created by Harlan Kellaway on 8/12/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SentenceGenerator : NSObject
+
+@end
---
+++ harlankellaway.spritebuilder/Source/SentenceGenerator.m
@@ -0,0 +1,13 @@
+//
+//  SentenceGenerator.m
+//  harlankellaway
+//
+//  Created by Harlan Kellaway on 8/12/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "SentenceGenerator.h"
+
+@implementation SentenceGenerator
+
+@end
